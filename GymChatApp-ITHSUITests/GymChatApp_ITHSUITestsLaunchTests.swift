0b5858435a1e401244889ec98e1036b4fdc4e2c1
David <David>
2023-02-07 10:34:13 +0100

    Initial Commit

---
+++ GymChatApp-ITHSUITests/GymChatApp_ITHSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GymChatApp_ITHSUITestsLaunchTests.swift
+//  GymChatApp-ITHSUITests
+//
+//  Created by David Lindström iths on 2/7/23.
+//
+
+import XCTest
+
+final class GymChatApp_ITHSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
