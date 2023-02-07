e3a312f1944630e73f74e755c7614095eff4d5ae
David <David>
2023-02-07 10:34:13 +0100

    Initial Commit

---
+++ GymChatApp-ITHS/GymChatApp_ITHSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymChatApp_ITHSApp.swift
+//  GymChatApp-ITHS
+//
+//  Created by David Lindström iths on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GymChatApp_ITHSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
