1a830d24997c4d2d86fd7b90b022eadc7f1298c8
David <David>
2023-02-23 16:46:21 +0100

    ´klar´

---
+++ GymChattingApp/GymChattingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymChattingAppApp.swift
+//  GymChattingApp
+//
+//  Created by David Lindström iths on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GymChattingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
